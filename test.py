5a595256798e81b66b39f527b44f1fa036d805c1
Liangsheng <liangshe@princeton.edu>
2015-03-26 16:35:22 -0400

    Extract and impute data in test

--- test.py
+++ test.py
@@ -17,4 +17,20 @@ hour, minute, second = pr.time_process(read_time)
 print '\n'
 print 'Loading time: ' + str(hour) + "h " + str(minute) + "m " + str(second) + "s "
 
-print chr1.data_map["train"][0]
+start_time = time.time()
+chr1.data_extract(True)
+read_time = time.time() - start_time
+hour, minute, second = pr.time_process(read_time)
+print '\n'
+print 'Processing time: ' + str(hour) + "h " + str(minute) + "m " + str(second) + "s "
+
+from sklearn import linear_model
+clf = linear_model.LinearRegression()
+
+from sklearn import preprocessing
+imputer = preprocessing.Imputer(copy=False)
+imputer.fit_transform(chr1.train_beta)
+
+print np.mean(chr1.train_beta)
+
+#beta_mean = np.mean(chr1.train[4:36])
