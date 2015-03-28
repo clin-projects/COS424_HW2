2de9d6a0d02629f4d3bc828da1afe56ad69860f1
Liangsheng <liangshe@princeton.edu>
2015-03-28 18:53:21 -0400

    Implement logistic-like plain regression

---
+++ logistic.py
@@ -0,0 +1,70 @@
+__author__ = 'liangshengzhang'
+
+import process as pr
+import numpy as np
+import time
+from math import log, exp
+
+start_time = time.time()
+
+chr1 = pr.Data(1)
+
+chr1.read()
+
+read_time = time.time() - start_time
+
+hour, minute, second = pr.time_process(read_time)
+
+print '\n'
+print 'Loading time: ' + str(hour) + "h " + str(minute) + "m " + str(second) + "s "
+
+start_time = time.time()
+chr1.data_extract(strand_binary=True, pos_normalize=True)
+
+from sklearn import preprocessing
+imputer = preprocessing.Imputer(copy=False)
+imputer.fit_transform(chr1.train_beta)
+
+process_time = time.time() - start_time
+hour, minute, second = pr.time_process(process_time)
+print '\n'
+print 'Processing time: ' + str(hour) + "h " + str(minute) + "m " + str(second) + "s "
+
+start_time = time.time()
+from sklearn import linear_model
+
+predict = []
+train_X = np.transpose(chr1.train_beta[chr1.sample_not_nan,:])
+sample_X = chr1.sample_beta[chr1.sample_not_nan]
+clf = linear_model.LinearRegression()
+
+
+for n in chr1.sample_nan:
+    data = [log(1.0/x-1) for x in chr1.train_beta[n,:]]
+    if n % 1000 == 0:
+        print n
+    clf.fit(train_X, data)
+    predict.append(clf.predict(sample_X))
+
+predict_time = time.time() - start_time
+hour, minute, second = pr.time_process(predict_time)
+print '\n'
+print 'Fitting and Predicting time: ' + str(hour) + "h " + str(minute) + "m " + str(second) + "s "
+
+
+# Normalized square error for prediction
+err = 0
+test_not_nan = []
+for n in range(len(predict)):
+    if not np.isnan(chr1.test_beta[chr1.sample_nan[n]]):
+        err += ((1.0/(1.0+exp(predict[n]))) - chr1.test_beta[chr1.sample_nan[n]])**2
+        test_not_nan.append(chr1.sample_nan[n])
+err = err / len(test_not_nan)
+
+# Varaince of the test data used for comparison
+var = np.var(chr1.test_beta[np.array(test_not_nan)])
+
+print '\n'
+print "Prediction Error Square:", err
+print "Error percentage:", err/var
+
